519542d5dc6014ebb18aa3abcf5c3597dcd16be1
songhee <etesong0114@gmail.com>
2023-07-13 15:20:51 +0900

    성별 랜덤 뽑기

---
+++ generators/gender_generator.py
@@ -0,0 +1,7 @@
+import random
+
+class GenderGenerate:
+    def generate_gender(self):
+        generated_gender = random.choice(["Male", "Female"])
+        # print(generated_gender)
+        return generated_gender
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from generators.name_generator import NameGenerate
 from generators.addr_generator import AddrGenerate
+from generators.gender_generator import GenderGenerate
+
+
 
 def main():
     name = NameGenerate()
     name.generate_name()
     addr = AddrGenerate()
     addr.generate_addr()
+    gender = GenderGenerate()
+    gender.generate_gender()
 
 if __name__ == "__main__":
     main()
